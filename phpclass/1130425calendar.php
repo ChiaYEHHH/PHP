eb447616769c74f085a15219ef9af969dff7ab92
ChiaYEHHH <hedy821216@gmail.com>
2024-04-26 16:36:16 +0800

    月曆運算式未完善

--- phpclass/1130425calendar.php
+++ phpclass/1130425calendar.php
@@ -124,7 +124,7 @@
 
     <?php
     $year=2024;
-    $mon=4;
+    $mon=5;
     
     $day1=strtotime("$year-$mon-01");
     echo "當月第一天". date("Y-m-d", $day1)."<br>";
@@ -136,14 +136,25 @@
     $md= date("t", $day1);
     echo "當月天數" .$md. "天";
     echo "<table>";
-    for($i=1 ; $i<=$md ; $i++){
-        if($i < $md){
+    for($i=0 ; $i<=$md ; $i++){
+        echo "<tr>";
+        
+        if($i < $wd1){
             // 印出1號之前的格子
             echo "<td>owo</td>";
-        }else if($i==$md){
+        }else if($i<=$md){
             // 印出個子相對應的日期
+            echo "<td>$days</td>";
+            $days++;
         }
-    }
+        if($i % 7 === 0){
+            echo "</tr>";
+            // echo "<tr>";
+    
+            }
+        
+
+    }echo "</tr>";
 
 
 
